1aef83cc3b815a34e0426228764109c7c45beee4
Tijani Sylla <tijani.sylla1@gmail.com>
2023-01-10 12:18:48 -0600

    more testing5

--- app.py
+++ app.py
@@ -39,7 +39,6 @@ def main():
     return app      
               
 if __name__ == '__main__': 
-         from waitress import serve
          app = main()
          app.run( host='0.0.0.0',port=8000)
         # localhost:8000/api
